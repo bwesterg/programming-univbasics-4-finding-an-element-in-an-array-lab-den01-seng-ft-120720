dad5bdd7cf44138a26c19cde0aaee5787bdee303
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-20 19:10:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,20 +1,3 @@
 def find_element_index(array, value_to_find)
-  count = 0
-  found_value_index = nil
-  while count < array.length do
-    if array[count] == value_to_find
-      found_value_index = count
-    end
-    count += 1
-  end
-  found_value_index
-end
-
-
-def find_element_index(array, value_to_find)
-  count = 0
-  while count < array.length do
-    puts array[count] == value_to_find
-    count += 1
-  end
+  array.index(value_to_find)
 end
